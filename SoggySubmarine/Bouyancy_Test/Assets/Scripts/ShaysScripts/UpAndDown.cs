2684a5621940f8aab4fb1fb3145e8a0c4fdfba26
Blake Laprade <lapradeblake@gmail.com>
2023-09-13 11:31:56 -0600

    Scene changes
    added boat sprite
    added ground
    (WIP) sub angle

--- SoggySubmarine/Bouyancy_Test/Assets/Scripts/ShaysScripts/UpAndDown.cs
+++ SoggySubmarine/Bouyancy_Test/Assets/Scripts/ShaysScripts/UpAndDown.cs
@@ -7,16 +7,21 @@ public class UpAndDown : MonoBehaviour
     private Rigidbody2D _body;
     [SerializeField] float _downForce = 22.53f;
     [SerializeField] private Floaty _bouyancy;
+    [SerializeField] private float _angle;
     
 
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+
     }
 
     void FixedUpdate()
     {
         HoldDown();
+
+        //angle boat. angle front towards vector. issues: no forward vector so would need to do math with a fake forward vector to calculate angle. 
+        //AngleShip();
     }
 
     void HoldDown()
@@ -31,5 +36,15 @@ public class UpAndDown : MonoBehaviour
             }
            
         }
+    }
+
+    void AngleShip()
+    {
+        _angle = (Vector2.Angle(new Vector2 (0,_body.velocity.y) , new Vector2(1, 0))) ;
+        //this.transform.Rotate(0,0, _angle);
+        transform.LookAt(transform.position + new Vector3(0,0,_angle));
+        
+        Debug.Log(_angle/50);
+        
     }
 }
