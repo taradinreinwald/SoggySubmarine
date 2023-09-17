e5bc28ac0afe24da7a358bed794f654e71bf9bb8
ShaylynKM <123325738+ShaylynKM@users.noreply.github.com>
2023-09-17 17:58:20 -0600

    Update PauseMenu.cs
    Added the newer code

--- ShaysScripts/PauseMenu.cs
+++ ShaysScripts/PauseMenu.cs
@@ -3,82 +3,120 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+ 
+
 public class PauseMenu : MonoBehaviour
 {
-    // create this object and attach the script to it.
     public GameObject PauseMenuUI;
+    public bool GamePaused;
 
-    public bool GamePaused = false;
+ 
 
-    void Start()
+    protected virtual void Awake()
     {
-        // If I didn't set the timeScale to 1, the game would freeze upon restart. This fixed it.
-        Time.timeScale = 1f;
-
-        // Making sure the game starts un-paused.
+        // Flags the game as not paused
         GamePaused = false;
     }
 
+ 
+
+    void Start()
+    {
+        // Time is normal when the game starts
+        Time.timeScale = 1f;
+
+ 
+
+        // Starts the game with the soundtrack playing
+        AudioManager.Instance.PlayUnderwater();
+    }
+
+ 
+
     void Update()
     {
-        // Uses the Escape key to pause the game.
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(GamePaused)
             {
-                // The game will not pause if it's already paused.
                 Resume();
             }
             else
             {
-                // Otherwise, it will pause.
                 Pause();
             }
         }
     }
 
-    // Public so that it can be accessed through the "resume" button.
+ 
+
     public void Resume()
     {
-        // Disables the pause menu.
+        // Disables the pause menu
         PauseMenuUI.SetActive(false);
 
-        // Unfreezes the game.
+ 
+
+        // Unfreezes the game
         Time.timeScale = 1f;
 
-        // Flags the game as being un-paused.
+ 
+
+        // Flags the game as not paused
         GamePaused = false;
+
+ 
+
+        // Plays the game soundtrack
+        AudioManager.Instance.PlayUnderwater();
+
+ 
+
+        // Stops the pause menu music
+        AudioManager.Instance.StopMainAndPause();
     }
 
-   
-    void Pause()
+ 
+
+    public void Pause()
     {
-        // Enables the pause menu.
+        // Enables the pause menu
         PauseMenuUI.SetActive(true);
 
-        // Freezes the game.
+ 
+
+        // Freezes the game
         Time.timeScale = 0f;
 
-        // Flags the game as being paused.
+ 
+
+        // Flags the game as paused
         GamePaused = true;
+
+ 
+
+        // Pauses the game soundtrack
+        AudioManager.Instance.PauseUnderwater();
+
+ 
+
+        // Plays the pause menu music
+        AudioManager.Instance.PlayMainAndPause();
     }
 
-    // These two functions are public so they can be accessed by their respective buttons.
+ 
+
     public void RestartGame()
     {
-        //Reloads the scene.
+        // Reloads the scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+ 
+
     public void QuitGame()
     {
-        //This is for quitting if the game was built.
         Application.Quit();
-
-        //This is for quitting in Play Mode within the editor. DELETE AFTER WE BUILD THE GAME.
-        UnityEditor.EditorApplication.isPlaying = false;
-
         Debug.Log("Quitting game...");
-
     }
 }
